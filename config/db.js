e3bb01b6b93debc79b1eaa077ee58aff64614011
unknown <amit1291997@gmail.com>
2019-12-09 19:21:07 +0530

    Did our first user registration and stored it on MongoDb Atlas User Schema

--- config/db.js
+++ config/db.js
@@ -6,11 +6,12 @@ const connectDB = async () => {
   try{
     await mongoose.connect(db, {
       useNewUrlParser: true,
+      useCreateIndex: true,
       useUnifiedTopology: true
     });
     console.log("MongoDB Connected...");
   } catch(err){
-    console.err(err.message);
+    console.error(err.message);
     // Exit process with failure
     process.exit(1);
   }
